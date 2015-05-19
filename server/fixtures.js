a92e4ffb3fa56042d65da11c89e19f8caeb0df5e
Nicholas Smith <npsmith1990@gmail.com>
2015-05-19 13:45:19 -0500

    Begin using iron router

--- server/fixtures.js
+++ server/fixtures.js
@@ -1,14 +1,29 @@
 if (Posts.find().count() === 0) {
   Posts.insert({
     title: 'Introducing Telescope',
-    url: 'http://sachagreif.com/introducing-telescope'
+    url: 'http://sachagreif.com/introducing-telescope',
+    author: 'bob-smith',
+    category: 'JavaScript',
+    flagged: false
   });
   Posts.insert({
     title: 'Meteor',
-    url: 'http://meteor.com'
+    url: 'http://meteor.com',
+    author: 'bob-smith',
+    category: 'JavaScript',
+    flagged: false
   });
   Posts.insert({
     title: 'The Meteor Book',
-    url: 'http://themeteorbook.com'
+    url: 'http://themeteorbook.com',
+    author: 'will-smith',
+    category: 'JavaScript',
+    flagged: false
+  });
+  Posts.insert({
+    title: 'The Flagged Book',
+    url: 'http://flag.com',
+    author: 'jim-finnegan',
+    flagged: true
   });
 }
---
+++ server/publications.js
@@ -0,0 +1,11 @@
+Meteor.publish('posts', function (author) {
+  return Posts.find({
+    flagged: false,
+    author: author
+  },
+  {
+    fields: {
+      flagged: false
+    }
+  });
+});
