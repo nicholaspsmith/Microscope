e5b049088179831b2d557956797bb50f79daa949
Nicholas Smith <npsmith1990@gmail.com>
2015-05-21 13:04:05 -0500

    add an autorun tracker

--- client/templates/application/layout.js
+++ client/templates/application/layout.js
@@ -1,3 +1,11 @@
 Template.layout.helpers({
   pageTitle: function () { return Session.get('pageTitle'); }
-})
+});
+
+
+Tracker.autorun( function() {
+  var newTitle = Session.get('pageTitle');
+  console.log('Value is: ' + newTitle);
+  // alert('The new title is: \"' + newTitle + '\"');
+  
+});
