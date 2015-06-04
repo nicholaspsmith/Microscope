55e5797608a6f28bf34fb0479fcadee1d4fbdc96
Nick P Smith <npsmith1990@gmail.com>
2015-06-03 23:31:40 -0600

    Add meteor method for sanitizing input

--- client/templates/posts/post_submit.js
+++ client/templates/posts/post_submit.js
@@ -4,11 +4,15 @@ Template.postSubmit.events({
 
     var post = {
       url: $(e.target).find('[name=url]').val(),
-      title: $(e.target).find('[name=title]').val(),
-      user: Meteor.user()
+      title: $(e.target).find('[name=title]').val()
     };
 
-    post._id = Posts.insert(post);
-    Router.go('postPage', post);
+    Meteor.call('postInsert', post, function(error, result) {
+      // display the error to the user and abort
+      if (error) {
+        return alert(error.reason);
+      }
+      Router.go('postPage', {_id: result._id});
+    });
   }
-})
+});
