12d0879e0f065457711912f1f1cc55c95b9fa263
Nick P Smith <npsmith1990@gmail.com>
2015-08-29 11:40:29 -0500

    add basic error messages

--- server/fixtures.js
+++ server/fixtures.js
@@ -2,28 +2,28 @@ if (Posts.find().count() === 0) {
   Posts.insert({
     title: 'Introducing Telescope',
     url: 'http://sachagreif.com/introducing-telescope',
-    author: 'bob-smith',
+    author: 'Bob Smith',
     category: 'JavaScript',
     flagged: false
   });
   Posts.insert({
     title: 'Meteor',
     url: 'http://meteor.com',
-    author: 'bob-smith',
+    author: 'Bob Smith',
     category: 'JavaScript',
     flagged: false
   });
   Posts.insert({
     title: 'The Meteor Book',
     url: 'http://themeteorbook.com',
-    author: 'will-smith',
+    author: 'Nick Smith',
     category: 'JavaScript',
     flagged: false
   });
   Posts.insert({
     title: 'The Flagged Book',
     url: 'http://flag.com',
-    author: 'jim-finnegan',
+    author: 'Fim Finnegan',
     flagged: true
   });
 }
